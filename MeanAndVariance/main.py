07be16724a7b3822405d33520612a7fc6a10932d
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-07-04 23:13:16 +0200

    started working on report

--- MeanAndVariance/main.py
+++ MeanAndVariance/main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 import math
+import numpy.random as random
 
 
 
@@ -127,7 +128,7 @@ if __name__ == '__main__':
     S_squared = sample_variance(samples_served_register_withdrawals, X_signed)
     S = math.sqrt(S_squared)
     print("sample variance of withdrawals = " + str(S_squared) + ", standard deviation = " + str(S))
-    show_normal(X_signed, S)
+    #show_normal(X_signed, S)
 
     #study of the samples given by observing deposits from the served register
     samples_served_register_deposits = "500 - 8010 - 7600 - 1200 - 1500 - 1000 - 400 - 340 - 500 - 780 - 500 - 1250 - 3700 - 1100 - 3000 - 2100 - 3000 - 1500 - 550 - 1700 - 2000 - 5000 - 1500 - 650 - 3080 - 300 - 3000 - 1900 - 800 - 3500 - 1050 - 100 - 1800 - 1640 - 2800 - 160 - 955 - 350 - 1100 - 750 - 3550 - 700 - 530"
@@ -136,7 +137,7 @@ if __name__ == '__main__':
     S_squared = sample_variance(samples_served_register_deposits, X_signed)
     S = math.sqrt(S_squared)
     print("sample variance of deposits = " + str(S_squared) + ", standard deviation = " + str(S))
-    show_normal(X_signed, S)
+    #show_normal(X_signed, S)
 
     #We also mention that, from some samples, we have measured that the customers that go to the served register
     #to carry out an operation have a 70% probability of doing a withdrawal and a 30% probability of doing a deposit.
@@ -156,7 +157,7 @@ if __name__ == '__main__':
     S_squared = sample_variance(samples_withdrawals_register, X_signed)
     S = math.sqrt(S_squared)
     print("sample variance of deposits = " + str(S_squared) + ", standard deviation = " + str(S))
-    show_normal(X_signed, S)
+    #show_normal(X_signed, S)
 
     # -------------------------------------------------------------------------------------
     # ---------------------Special register: Withdrawals and Deposits----------------------
@@ -173,7 +174,7 @@ if __name__ == '__main__':
     S_squared = sample_variance(samples_special_register_withdrawals, X_signed)
     S = math.sqrt(S_squared)
     print("sample variance of withdrawals = " + str(S_squared) + ", standard deviation = " + str(S))
-    show_normal(X_signed, S)
+    #show_normal(X_signed, S)
 
     #study of the samples given by observing deposits from the special register
     samples_special_register_deposits = "200 - 600 - 930 - 2800 - 50 - 40 - 600 - 420 - 1100 - 10350 - 2210 - 70 - 450 - 1900 - 80 - 1980 - 410 - 130 - 15000 - 800 - 200 - 400 - 700 - 7000 - 8855 - 6850 - 400 - 2160 - 2000 - 1290 - 400 - 1000 - 130 - 105 - 950 - 3000 - 2800 - 1000 - 20 - 2900 - 800 - 1200 - 1050 - 50 - 330 - 600 - 40 - 960 - 320 - 4450 - 170 - 50 - 1000 - 350 - 150"
@@ -184,7 +185,40 @@ if __name__ == '__main__':
     S_squared = sample_variance(samples_special_register_deposits, X_signed)
     S = math.sqrt(S_squared)
     print("sample variance of deposits = " + str(S_squared) + ", standard deviation = " + str(S))
-    show_normal(X_signed, S)
-    
+    #show_normal(X_signed, S)
 
+    #we know that, from 8.30 to 13.00, an average of 40 customers arrive to the bank branch.
+    #We assume, for simplicity, to model the arrival times of the customers with a poisson process.
+    #Let's take as unit of time, for our modelling, the minutes: the rate Lambda at which a customer
+    #arrives per minute, in this interval of time, is 40/270 (approx. 0.148)
+    #let's try to generate the instants in time in which the customers arrive with an algorithm for
+    #simulating a poisson process.
+    lambda_served_customers = 40/270
+    T = 60*13       #13:00
+    i = 0
+    t = 60*8 + 30   #8:30
+    while t < T:
+        i = i + 1
+        s = random.exponential(1/lambda_served_customers)
+        t += s
+        if t < T:
+            h = t//60
+            m = t%60
+            print("the %d th customer arrived at time %d : %d" %(i, h, m))
 
+    #Now let's do the same for the arrival of customers that want no use one of the ATMs.
+    #Experience showed that in a solar day (so, from 00:00 to 00:00 of the following day), an average
+    #of 300 customers show up to use one of the ATMs. Also in this case, we can model their arrival
+    #times with a poisson process.
+    lambda_atm_customers = 300 / 1440
+    T = 1440    #00:00 of the following day
+    i = 0
+    t = 0       #00:00 of the current day
+    while t < T:
+        i = i + 1
+        s = random.exponential(1 / lambda_atm_customers)
+        t += s
+        if t < T:
+            h = t // 60
+            m = t % 60
+            print("the %d th customer arrived at time %d : %d" % (i, h, m))
